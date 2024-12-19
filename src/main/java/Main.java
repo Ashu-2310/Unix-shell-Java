afb268493c05aea7d1bd6815d8c8385fbc4ef083
Ashu-2310 <ashutoshpande35@gmail.com>
2024-12-19 15:42:56 +0530

    type echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void echoCommand(String input)
     {
         System.out.println(input);
+    }
+    
+    public static void checkCommandType(String input){
+        ArrayList<String> listOfCommands = new ArrayList<>(Arrays.asList("echo","ls","cd","type","pwd","rm","mkdir","exit"));
+        if(listOfCommands.contains(input)){
+            System.out.println(input + " is a shell builtin");
+        }
+        else{
+            System.out.println(input + ": command not found");
+        }
     }
     
 	public static void main(String[] args) throws Exception {
@@ -20,6 +30,9 @@ public class Main {
 			if(input.startsWith("echo"))
 			{
 			    echoCommand(input.substring(5));
+			}
+			else if(input.startsWith("type")){
+			    checkCommandType(input.substring(5));
 			}
 			else{
 			    System.out.println(input + ": command not found");
