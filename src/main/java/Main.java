b639616dcf1138c4517cbd03cf21da1adbd5ed33
Ashu-2310 <ashutoshpande35@gmail.com>
2024-12-19 12:51:20 +0530

    implemnt echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        while(true){
-        	System.out.print("$ ");
+	public static void main(String[] args) throws Exception {
+		// Uncomment this block to pass the first stage
+		while(true) {
+			System.out.print("$ ");
 
-        	Scanner scanner = new Scanner(System.in);
-        	String input = scanner.nextLine();
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
 
-		if(input.equals("exit 0")){
-           	break;
-           }
-
-           for(int i=0; i<input.length; i++){
-           	String comm = "";
-           	if(input.charAt(i) != ' '){
-           		comm += input.charAt(i);
-           	}
-			else{
-				if(comm.equals("echo"){
-					System.out.println(input.substring(i+1);
-					break;
+			if(input.equals("exit 0")) {
+				break;
 			}
-		}
+			String comm = "";
+			for(int i=0; i<input.length(); i++) {
+				if(input.charAt(i) != ' ') {
+					comm += input.charAt(i);
+				}
+				else {
+					if(comm.equals("echo")) {
+						System.out.println(input.substring(i+1));
+						break;
+					}
+				}
+			}
 
-        	System.out.println(input + ": command not found");
-       }
-    }
+			System.out.println(input + ": command not found");
+		}
+	}
 }
